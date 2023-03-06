b858537ce67a701aa4b0519d604249e4d92afbb2
Simon Vargas <sjvargas01@gmail.com>
2023-03-05 19:20:35 -0500

    first commit

---
+++ src/main/java/com/futurex/services/FutureXUserApp/FutureXUserAppApplication.java
@@ -0,0 +1,14 @@
+package com.futurex.services.FutureXUserApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureXUserAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FutureXUserAppApplication.class, args);
+	}
+
+}
